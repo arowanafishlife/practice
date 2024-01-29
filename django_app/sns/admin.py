06f8e3cf9dc316031e85f88d1561f1a898b620fe
arowanafishlife <miyamotoy0604@gmail.com>
2024-01-30 02:10:51 +0900

    sns for Django practice, first commit

--- django_app/sns/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Message, Good, Following, Profile_text
-
-# Register your models here.
-admin.site.register(Message)
-admin.site.register(Good)
-admin.site.register(Following)
-admin.site.register(Profile_text)
